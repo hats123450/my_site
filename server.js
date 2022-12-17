27903e08091a7a8ece15a1fdf9f1bb96db041819
ak47 king <78044743+hats123450@users.noreply.github.com>
2022-12-17 18:48:06 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var Pusher = require('pusher');
+
+var pusher = new Pusher({
+  appId: '303964',
+  key: '818bd080-acc6-42a1-bde8-523cfd50bf41',
+  secret: '5E06A5DA7182360C83B414C06DFDBD07112175402384D1544F706F0C20E5E523',
+  cluster: 'ap2',
+  encrypted: true
+});
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/comment', function(req, res){
+  console.log(req.body);
+  var newComment = {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment
+  }
+  pusher.trigger('flash-comments', 'new_comment', newComment);
+  res.json({  created: true });
+});
+
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+    var error404 = new Error('Route Not Found');
+    error404.status = 404;
+    next(error404);
+});
+
+module.exports = app;
+
+app.listen(9000, function(){
+  console.log('Example app listening on port 9000!')
+});
